05e0d5757f5c8cd487e9a64776583c428b99f221
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-03 07:42:41 +0900

    feat: タスク作成機能

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { db } from './firebase'
+import { FormControl, TextField } from '@material-ui/core'
+import { AddToPhotosOutlined } from '@material-ui/icons'
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: '', title: '' }])
+  const [input, setInput] = useState('')
+
   useEffect(() => {
     const unSub = db
       .collection('tasks')
@@ -15,8 +19,29 @@ const App: React.FC = () => {
     return () => unSub()
   }, [])
 
+  const newTask = () => {
+    db.collection('tasks').add({ title: input })
+    setInput('')
+  }
+
   return (
     <div className="App">
+      <h1>Todo App by React/Firebase</h1>
+
+      <FormControl>
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          label="Nre task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosOutlined />
+      </button>
+
       {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
       ))}
