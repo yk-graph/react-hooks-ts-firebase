87bc750045b4f89313746e17cc60a89433969198
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-03 15:35:31 +0900

    feat: CRUD

--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { ListItem } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Grid, ListItem, TextField } from '@material-ui/core'
+import { DeleteOutlined, EditOutlined } from '@material-ui/icons'
+import { db } from './firebase'
 
 interface Props {
   id: string
@@ -7,9 +9,35 @@ interface Props {
 }
 
 const TaskItem: React.FC<Props> = (props) => {
+  const [title, setTitle] = useState(props.title)
+
+  const editTask = () => {
+    db.collection('tasks').doc(props.id).set({ title: title }, { merge: true })
+  }
+
+  const deleteTask = () => {
+    db.collection('tasks').doc(props.id).delete()
+  }
+
   return (
     <ListItem>
       <h2>{props.title}</h2>
+      <Grid container justifyContent="flex-end">
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          value={title}
+          label="Edit Task"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+        <button>
+          <EditOutlined onClick={editTask} />
+        </button>
+        <button>
+          <DeleteOutlined onClick={deleteTask} />
+        </button>
+      </Grid>
     </ListItem>
   )
 }
