c7de145eada7e4ab877a49c0ab676dad9129da31
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-03 15:16:17 +0900

    add: TaskItem コンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { db } from './firebase'
-import { FormControl, TextField } from '@material-ui/core'
+import { FormControl, List, TextField } from '@material-ui/core'
 import { AddToPhotosOutlined } from '@material-ui/icons'
+import TaskItem from './TaskItem'
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: '', title: '' }])
@@ -42,9 +43,11 @@ const App: React.FC = () => {
         <AddToPhotosOutlined />
       </button>
 
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.title}</h3>
-      ))}
+      <List>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
+      </List>
     </div>
   )
 }
