823c9c9fd475bc26182d46a791261380b05da9d0
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-04 11:18:46 +0900

    add: firebase config setting

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+//Firebase ver9 compliant (modular)
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+})
+
+//Firebase ver9 compliant (modular)
+export const auth = getAuth(firebaseApp)
+export const db = getFirestore(firebaseApp)
