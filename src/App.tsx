e00aebd6651de5c30f06106793ab7c84a6665fb2
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-05 11:25:47 +0900

    add: style

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import React, { useState, useEffect } from 'react'
 import TaskItem from './TaskItem'
 import { db } from './firebase'
 import { addDoc, collection, onSnapshot, query } from 'firebase/firestore'
-import { FormControl, TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import { FormControl, List, TextField } from '@material-ui/core'
 import { AddToPhotosOutlined } from '@material-ui/icons'
-import './App.css'
+import styles from './App.module.css'
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: 'auto',
+    width: '40%',
+  },
+})
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: '', title: '' }])
   const [input, setInput] = useState('')
+  const classes = useStyles()
 
   // 初回のみ情報を取得したいので、第二引数は[]にしておく
   useEffect(() => {
@@ -42,11 +55,12 @@ const App: React.FC = () => {
   }
 
   return (
-    <div className="App">
+    <div className={styles.app__root}>
       <h1>Todo App by React/Firebase</h1>
 
       <FormControl>
         <TextField
+          className={classes.field}
           InputLabelProps={{ shrink: true }}
           label="New Task"
           value={input}
@@ -55,13 +69,15 @@ const App: React.FC = () => {
           }
         />
       </FormControl>
-      <button disabled={!input} onClick={addTask}>
+      <button className={styles.app__icon} disabled={!input} onClick={addTask}>
         <AddToPhotosOutlined />
       </button>
 
-      {tasks.map((task) => (
-        <TaskItem key={task.id} id={task.id} title={task.title} />
-      ))}
+      <List className={classes.list}>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
+      </List>
     </div>
   )
 }
