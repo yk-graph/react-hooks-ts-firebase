e1186be0ce085411fd14c9e89c1a89524261729f
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-19 12:31:16 +0900

    feat: fetch task & create task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,60 @@
-import React, { memo } from 'react'
+import React, { memo, useState, useEffect } from 'react'
+import { addDoc, collection, query, onSnapshot } from 'firebase/firestore'
+import { FormControl, TextField } from '@mui/material'
+import { AddToPhotosOutlined } from '@mui/icons-material'
+
+import { db } from './firebase'
+
+interface Task {
+  id: string
+  title: string
+}
 
 const App: React.FC = () => {
-  return <div className="App"></div>
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    const q = query(collection(db, 'tasks'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setTasks(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().title,
+        }))
+      )
+    })
+    return () => unsubscribe()
+  }, [])
+
+  const newTask = async () => {
+    await addDoc(collection(db, 'tasks'), {
+      title: input,
+    })
+    setInput('')
+  }
+
+  return (
+    <div>
+      <h1>Todo App by React</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          label="New Task"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosOutlined />
+      </button>
+      {tasks.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+    </div>
+  )
 }
 
 export default memo(App)
---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+})
+
+export const auth = getAuth(firebaseApp)
+export const db = getFirestore(firebaseApp)
